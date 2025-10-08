c78ac7d4b4239ad14ea8826c91a1417c3932a441
erickgfs <erickgfs@gmail.com>
2025-10-07 21:11:18 -0300

    feat:setup initial expresse server and links API endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
+import { useState, useEffect } from 'react';
+
 import { Profile } from "./components/Profile";
 import { LinkButton } from "./components/LinkButton";
 import { SocialIcons } from "./components/SocialIcons";
 import "./globals.css"
 
 export function App() {
+  const [myLinks, setMyLinks] = useState({});
 
-  const myLinks = {
-    links: [
-      {id: 1, title: "Github", url: "https://github.com/erickgfs"},
-      {id: 2, title: "Linkedin", url: "https://www.linkedin.com/in/erickgfs"}
-    ],
-    social: [
-      {id: 1, title: "Instagram", url: "https://www.instagram.com/erick.kroghar"},
-      {id: 2, title: "Facebook", url: "https://www.facebook.com/erickgfs"}
-    ]
-  }
+  useEffect(() => {
+    const fetchLinks = async () => {
+      try {
+
+        const response = await fetch("http://localhost:3001/api/links");
+        const data = await response.json();
+        console.log(data)
+
+        setMyLinks(data);
+      } catch (error) {
+        console.log(`Erro ao buscar dados da API: ${error}`);
+      }
+
+    }
+
+    fetchLinks();
+  },[])
 
   return (
     <div className="bg-brand-dark min-h-screen flex flex-col items-center justify-center p-4">
       <Profile imgUrl="https://github.com/erickgfs.png" name="Erick Giovanni Fonseca Silva" profession="Desenvolvedor"/>
-      {myLinks.links.map( link => (
+      {myLinks.links && myLinks.links.map( link => (
         <LinkButton key={link.id} title={link.title} url={link.url}/>
       ))}
       <div className="flex flex-row gap-3 p-5">
-        {myLinks.social.map( link => (
+        {myLinks.social && myLinks.social.map( link => (
           <SocialIcons key={link.id} title={link.title} url={link.url}/>
         ))}
       </div>
