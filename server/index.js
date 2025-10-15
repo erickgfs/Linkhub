0331c5e3d0c423991c3658c48b4b80a674093528
erickgfs <erickgfs@gmail.com>
2025-10-15 19:54:38 -0300

    feat: implements POST endpoint to create links

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
+app.use(express.json());
 
 const prisma = new PrismaClient();
 
@@ -22,6 +23,29 @@ app.get('/api/links', async (req, res) => {
         res.status(500).json({ error: "Não foi possivel buscar os links." })
     }
 });
+
+app.post('/api/links', async (req, res) => {
+    const { title, url, type } = req.body;
+
+    if (!title || !url || !type) {
+        return res.status(400).json({ error: 'Titulo, URL e tipo são obrigatórios.'});
+    }
+
+    try {
+        const newLink = await prisma.link.create({
+            data: {
+                title: title,
+                url: url,
+                type: type
+            }
+        });
+
+        res.status(201).json(newLink);
+    } catch (error) {
+        console.error("Erro ao criar link:", error);
+        res.json(500).json(({ error: "Não foi possivel criar o link."}));
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
